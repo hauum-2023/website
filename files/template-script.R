eb6e1fad98b7761283d8d978309a30272649662d
weharris <51294972+weharris@users.noreply.github.com>
2023-03-28 10:03:58 +0200

    update

---
+++ files/template-script.R
@@ -0,0 +1,14 @@
+## HEADER ####
+## Who: <YOUR NAME>
+## What: 2 R language basics
+## Last edited: <DATE TODAY in yyyy-mm-dd format)
+####
+
+
+## CONTENTS ####
+## 00 Setup
+## 01 ...
+
+## 00 Setup  ####
+
+
